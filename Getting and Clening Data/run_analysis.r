250758db426dc7c4a4feb356652628fe4e6deb62
Pavlo Sai <sajpasha@gmail.com>
2017-01-11 11:55:18 +0200

    Add files via upload

---
+++ Getting and Clening Data/run_analysis.r
@@ -0,0 +1,6 @@
+require(knitr)
+require(markdown)
+# setwd("~/GitHub repositories/GettingAndCleaning")
+setwd("~/Repositories/Coursera/GettingAndCleaning")
+knit("run_analysis.Rmd", encoding="ISO8859-1")
+markdownToHTML("run_analysis.md", "run_analysis.html")
